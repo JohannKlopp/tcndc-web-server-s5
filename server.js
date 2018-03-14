1b91c22c96329a276493868ac5c0b0f31a76f0bc
Johann Klopp <clouduser@i-791.localdomain>
2018-03-14 13:07:40 +0000

    Add projects.hbs file, delete server.log, set ups for coming feature

--- server.js
+++ server.js
@@ -1,31 +1,25 @@
 const express = require('express');
 const hbs = require("hbs");
 const fs = require("fs"); // requiring the filesystem. Allows me to access the PC's filesystem and manipulate it
-
 const port = process.env.PORT || 3000; // default is 3000
+
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials"); // add support for partials
 app.set("view engine", "hbs"); // "handlebars" is a view engine for express, just like "pug"
 
 
-app.use( (req, res, next) => { //since we're doing sth async here, next() has to be called for the middleware to continue.
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
-  console.log(log);
-  fs.appendFile("server.log", log + "\n", (err) => {
-    if (err) {
-      console.log("Unable to append to server.log.");
-    }
-  });
-  next();
-});
-
-// app.use((req, res, next) => {
-//   res.render("maintenance.hbs");
+// app.use( (req, res, next) => { //since we're doing sth async here, next() has to be called for the middleware to continue.
+//   var now = new Date().toString();
+//   var log = `${now}: ${req.method} ${req.url}`;
+//   console.log(log);
+//   fs.appendFile("server.log", log + "\n", (err) => {
+//     if (err) {
+//       console.log("Unable to append to server.log.");
+//     }
+//   });
+//   next();
 // });
-
-app.use(express.static(__dirname + "/public")); // middleware for express to teach express how to read from a static directory
 
 hbs.registerHelper("getCurrentYear", () => {
   return new Date().getFullYear();
@@ -35,8 +29,7 @@ hbs.registerHelper("screamIt", (text) => {
   return text.toUpperCase();
 });
 
-app.get("/"/*this is the 'route'. the ending of the URL that points to this handler*/, (req, res) => {
-  //res.send("<h1>Hello Express!</h1>");
+app.get("/", (req, res) => {
   res.render("home.hbs", {
     welcomeMessage: "Welcome to this wonderful world!",
     pageTitle: "Home Page",
@@ -44,13 +37,12 @@ app.get("/"/*this is the 'route'. the ending of the URL that points to this hand
 });
 
 app.get("/about", (req, res) => {
-  res.render("about.hbs", { // rendering a separate html (view engine) file.
+  res.render("about.hbs", { // rendering a separate html file.
     pageTitle: "About Page",
   });
 });
 
 // /bad - send back JSON with errorMessage property "Error handling this request"
-
 app.get("/bad", (req, res) => {
   res.send({
     errorMessage: "Uups unable to handle this request."
